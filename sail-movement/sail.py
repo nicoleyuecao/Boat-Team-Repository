ec5276ef3079143f7b2a76460e9e7663231d3238
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2019-02-26 10:44:41 +0000

    UPDATE sail.py
    First upload of base sail code. 
    Needs to be tested on the boat before further elaboration.

--- sail-movement/sail.py
+++ sail-movement/sail.py
@@ -1,1 +1,28 @@
+import RPi.GPIO as GPIO
 
+def setupPi():
+
+    GPIO.setmode(GPIO.BCM)  # define use of BCM mode.
+    GPIO.setup(17, GPIO.OUT)  # Set pin 17 to be an output.
+    pinsetup = GPIO.PWM(17, 50)  # Pin and frequency of PWM.
+    if runPi(pinsetup) == "Success":
+        print("Sail turned")
+    else:
+        return "Sail turning failed."
+
+
+def runPi(pin):
+
+    try:
+        print("Attempting to set sail servo")
+        pin.start(1)  # Sets the initial position, this will need to be changed to be more accurate.
+    except:
+        print("Error")
+        pin.stop()
+        GPIO.cleanup()
+        return "Failed"
+    
+    return "Success"
+
+
+setupPi()
