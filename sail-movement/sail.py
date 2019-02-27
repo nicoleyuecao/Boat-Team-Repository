a7bd062aca5ef7e3a5f73a80a5a621a50b5f48a5
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2019-02-27 16:26:09 +0000

    UPDATED sail.py
    Configured auto change of the sail direction based on degree input.

--- sail-movement/sail.py
+++ sail-movement/sail.py
@@ -10,12 +10,14 @@ def setupPi():
     pinsetup = GPIO.PWM(12, 50)  # Pin and frequency of PWM.
     return pinsetup
 
-def runPi(pin):
+def runPi(pin, angle):
+    
+    dutyChange = int(angle/18) + 1
 
     try:
         print("Attempting to set sail servo")
-        pin.start(1)  # Sets the initial position, this will need to be changed to be more accurate.
-        pin.ChangeDutyCycle(7) # 7 is the middle point for the servo, 1 is the start, 12 is the end.
+        pin.start(0)  # Sets the initial position.
+        pin.ChangeDutyCycle(dutyChange) # 7 is the middle point for the servo, 1 is the start, 12 is the end.
         time.sleep(5) # Sleep until change is made.
     except:
         print("Error")
@@ -27,5 +29,15 @@ def runPi(pin):
     return "Success"
 
 
-setup = setupPi()
-runPi(setup)
+setup = setupPi() 
+runner = true
+
+sailAngle = int(input("Please enter the angle you would like the sail and then press enter: ")) #90 degrees as the angle is central starting location.
+
+"""
+while runner:
+    sailAngle = int(input("Please enter the angle you would like the sail and then press enter: ")) # 90 degrees as the angle is currently central starting location.
+    if sailAngle == "q" or sailAngle == "Q":
+        runner = false
+"""    
+runPi(setup, sailAngle)
