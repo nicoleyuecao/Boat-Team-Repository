1eb886efc6f07b11db67f694d39b2c09d9fd1e0b
GarethHolloway <33927441+GarethHolloway@users.noreply.github.com>
2019-02-27 15:14:07 +0000

    UPDATED sail.py
    Set the middle sail point, added sleep to prevent the code from conflicting with the actual servo movement. WORKING CODE.

--- sail-movement/sail.py
+++ sail-movement/sail.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 
 def setupPi():
 
-    GPIO.setmode(GPIO.BCM)  # define use of BCM mode.
-    GPIO.setup(17, GPIO.OUT)  # Set pin 17 to be an output.
-    pinsetup = GPIO.PWM(17, 50)  # Pin and frequency of PWM.
+    GPIO.setmode(GPIO.BOARD)  # define use of BCM mode.
+    GPIO.setup(12, GPIO.OUT)  # Set pin 17 to be an output.
+    pinsetup = GPIO.PWM(12, 50)  # Pin and frequency of PWM.
     return pinsetup
 
 def runPi(pin):
@@ -12,6 +12,8 @@ def runPi(pin):
     try:
         print("Attempting to set sail servo")
         pin.start(1)  # Sets the initial position, this will need to be changed to be more accurate.
+        pin.ChangeDutyCycle(7) # 7 is the middle point for the servo, 1 is the start, 12 is the end.
+        time.sleep(5) # Sleep until change is made.
     except:
         print("Error")
         return "Failed"
